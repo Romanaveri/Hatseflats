7e300b2487483b1d519e3c3bbd96fdc62fc0d431
roman <ranveri@roc-dev.com>
2016-09-09 14:04:37 +0200

    Opdracht 4.1

---
+++ src/H04/Opdracht4.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+import java.applet.*;
+
+public class Opdracht4 extends Applet {
+
+    public void init() {
+    }
+
+    public void paint(Graphics g) {
+        g.drawLine(20, 100, 100, 20);
+        g.drawLine(100, 20, 180, 100);
+        g.drawLine(20, 100, 180, 100);
+}
+}
