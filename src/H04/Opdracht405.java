4208aa9ef780b21941d340d2f268d9552839850d
roman <ranveri@roc-dev.com>
2016-09-18 18:20:35 +0200

    Opdracht 4.5

---
+++ src/H04/Opdracht405.java
@@ -0,0 +1,19 @@
+import java.awt.*;
+import java.applet.*;
+
+/**
+ * Created by roman on 15-9-2016.
+ */
+
+public class Opdracht405 extends Applet {
+
+    public void init() {
+    }
+
+    public void paint(Graphics g) {
+        setBackground(Color.BLUE);
+        g.setColor(Color.yellow);
+
+        g.fillOval(100, 120, 160, 50);
+    }
+}
