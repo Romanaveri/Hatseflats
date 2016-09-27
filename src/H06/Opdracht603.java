16227a6e2d1b7c562676525289e4530459fad7ba
roman <ranveri@roc-dev.com>
2016-09-27 13:02:34 +0200

    Opdracht 6

---
+++ src/H06/Opdracht603.java
@@ -0,0 +1,28 @@
+package H06;
+
+/**
+ * Created by roman on 27-9-2016.
+ */
+
+import java.awt.*;
+import java.applet.*;
+
+
+public class Opdracht603 extends Applet {
+    int a;
+    int b;
+    int c;
+    int uitkomst;
+
+
+    public void init() {
+        a = 20;
+        b = 15;
+        c = 15;
+        uitkomst = a - (b + c);
+
+    }
+    public void paint(Graphics g) {
+        g.drawString("Uitkomst: " + uitkomst, 20, 20);
+    }
+}
